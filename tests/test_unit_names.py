e2b3fe3ae94911dcf897f51204621ac3ce99bf07
Anna Wester <annawester73@gmail.com>
2026-04-06 12:40:57 +0200

    Commit with unit test TDD red and fixes in python-ci.yml and other configuration files

---
+++ tests/test_unit_names.py
@@ -0,0 +1,16 @@
+import pytest
+from src.names import name_exists, create_name_list
+
+@pytest.mark.unit
+def test_create_name_list_is_not_empty():
+    names = create_name_list()
+    assert len(names) > 0
+
+@pytest.mark.unit
+def test_name_exists_returns_true_for_existing_name():
+    assert name_exists("Anna") is True
+
+
+@pytest.mark.unit
+def test_name_exists_returns_false_for_missing_name():
+    assert name_exists("Sessan") is False
