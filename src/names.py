70c5acfe0876c64d6f7b198984262d2a655d6c73
Anna Wester <annawester73@gmail.com>
2026-04-06 12:49:54 +0200

    Commit with fixes får Lint code with flake8

---
+++ src/names.py
@@ -0,0 +1,10 @@
+def create_name_list():
+    pass
+
+
+def name_exists(name):
+    pass
+
+
+def get_namnes():
+    pass
--- tests/test_unit_names.py
+++ tests/test_unit_names.py
@@ -1,10 +1,12 @@
 import pytest
 from src.names import name_exists, create_name_list
 
+
 @pytest.mark.unit
 def test_create_name_list_is_not_empty():
     names = create_name_list()
     assert len(names) > 0
+
 
 @pytest.mark.unit
 def test_name_exists_returns_true_for_existing_name():
